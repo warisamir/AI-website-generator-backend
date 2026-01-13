f982f1d576a8c6be3c44418a362819c2c039e8de
warisamir <warisamir1918@gmail.com>
2026-01-13 08:54:59 +0530

    ading plan

--- src/main/java/com/rockhardy/lovable/service/SubscriptionService.java
+++ src/main/java/com/rockhardy/lovable/service/SubscriptionService.java
@@ -1,8 +1,15 @@
 package com.rockhardy.lovable.service;
+import com.rockhardy.lovable.Enum.SubscriptionStatus;
 import com.rockhardy.lovable.dto.subscription.CheckoutResponse;
 import com.rockhardy.lovable.dto.subscription.PortalResponse;
 import com.rockhardy.lovable.dto.subscription.SubscriptionResponse;
 
+import java.time.Instant;
+
 public interface SubscriptionService {
      SubscriptionResponse getCurrentSubscription() ;
-    }
+
+    void activateSubscription(Long userId, Long planId, String subscriptionId, String customerId);
+
+    void updateSubscription(String id, SubscriptionStatus status, Instant periodStart, Instant periodEnd, Boolean cancelAtPeriodEnd, Long planId);
+}
--- src/main/java/com/rockhardy/lovable/service/impl/SubscriptionServiceImpl.java
+++ src/main/java/com/rockhardy/lovable/service/impl/SubscriptionServiceImpl.java
@@ -1,5 +1,6 @@
 package com.rockhardy.lovable.service.impl;
 
+import com.rockhardy.lovable.Enum.SubscriptionStatus;
 import com.rockhardy.lovable.dto.subscription.CheckoutResponse;
 import com.rockhardy.lovable.dto.subscription.PortalResponse;
 import com.rockhardy.lovable.dto.subscription.SubscriptionResponse;
@@ -8,11 +9,23 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.time.Instant;
+
 @Service
 @Slf4j
 public class SubscriptionServiceImpl implements SubscriptionService {
     @Override
     public SubscriptionResponse getCurrentSubscription() {
         return null;
+    }
+
+    @Override
+    public void activateSubscription(Long userId, Long planId, String subscriptionId, String customerId) {
+
+    }
+
+    @Override
+    public void updateSubscription(String id, SubscriptionStatus status, Instant periodStart, Instant periodEnd, Boolean cancelAtPeriodEnd, Long planId) {
+
     }
 }
