146460df3caa9f3b103c3c539a4d42381e239fac
WarisAmir <warisamir1918@gmail.com>
2026-04-24 00:38:50 +0530

    adding docker-compose for pgvector

--- src/main/java/com/rockhardy/lovable/entity/ProjectFile.java
+++ src/main/java/com/rockhardy/lovable/entity/ProjectFile.java
@@ -1,23 +1,34 @@
 package com.rockhardy.lovable.entity;
 
+import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.experimental.FieldDefaults;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import org.springframework.data.annotation.CreatedDate;
 
 import java.time.Instant;
-
+@Entity
 @FieldDefaults(level= AccessLevel.PRIVATE)
 @Getter
 @Setter
 public class ProjectFile {
+        @Id
+        @GeneratedValue(strategy = GenerationType.IDENTITY)
         Long id;
+        @ManyToOne(fetch = FetchType.LAZY)
+        @JoinColumn(name = "project_id",nullable = false)
         Project project;
+        @Column(nullable = false)
         String path;
+
         String minioObjectKey;
+
+        @CreationTimestamp
         Instant createdAt;
+        @UpdateTimestamp
         Instant updatedAt;
-        User createdBy;
-        User updatedBy;
 
 }
